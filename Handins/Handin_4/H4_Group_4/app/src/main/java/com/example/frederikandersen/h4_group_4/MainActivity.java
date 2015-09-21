6ca77ba48e4ae597915680020b243815d4b808c3
Frederik Andersen <frederik@fafi.dk>
2015-09-21 16:22:17 +0200

    Handin_4 work

--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
@@ -1,37 +1,65 @@
 package com.example.frederikandersen.h4_group_4;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    WeatherService mService;
+    boolean mBound = false;
+
+    private TextView txtview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtview = (TextView) findViewById(R.id.textView);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart(){
+        super.onStart();
+
+        Intent intent = new Intent(this, WeatherService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+        //String mystring = mService.getWeatherJSON();
+
+
+        //txtview.setText(mystring);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStop(){
+        super.onStop();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            WeatherService.LocalBinder binder = (WeatherService.LocalBinder) iBinder;
+            mService = binder.getService();
+            mBound = true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBound = false;
+        }
+    };
 }
--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
@@ -2,15 +2,64 @@ package com.example.frederikandersen.h4_group_4;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
 
 public class WeatherService extends Service {
-    public WeatherService() {
+
+    private String HTTP_ADDRESS = "http://api.openweathermap.org/data/2.5/weather?id=2624652&units=metric";
+
+    private final IBinder mBinder = new LocalBinder();
+
+    public class LocalBinder extends Binder{
+        WeatherService getService(){
+            return WeatherService.this;
+        }
     }
+
+   public String getWeatherJSON() {
+
+       HttpURLConnection connection = null;
+
+       try {
+           URL url = new URL(HTTP_ADDRESS);
+           connection = (HttpURLConnection) url.openConnection();
+           connection.setRequestMethod("GET");
+           connection.connect();
+
+
+           BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+           StringBuilder stringBuilder = new StringBuilder();
+
+           String line;
+           while((line = bufferedReader.readLine()) != null){
+               stringBuilder.append((line+"\n"));
+           }
+           bufferedReader.close();
+           return stringBuilder.toString();
+       }
+       catch (MalformedURLException ex) {
+           Log.i("getWeatherJSON", "MalformedURLException");
+           return "";
+       }
+       catch (IOException ex){
+           Log.i("getWeatherJSON", "IOException");
+           return "";
+       }
+
+   }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
