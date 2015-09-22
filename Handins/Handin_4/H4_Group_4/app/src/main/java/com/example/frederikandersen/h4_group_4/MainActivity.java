9cbeecce9a361bbea10a2d18c724e1130beaf557
Frederik Andersen <frederik@fafi.dk>
2015-09-22 22:15:03 +0200

    Handin 4 almost done

--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.graphics.Bitmap;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
@@ -14,24 +15,43 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
     WeatherService mService;
     boolean mBound = false;
 
-    private TextView txtview;
+    private TextView txtCity;
+    private TextView txtUpdate;
+    private TextView txtWeatherInfo;
+    private TextView txtTemperature;
+    private TextView txtDescription;
+    private ImageView igvWeatherIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtview = (TextView) findViewById(R.id.textView);
+        txtCity = (TextView) findViewById(R.id.city);
+        txtUpdate = (TextView) findViewById(R.id.update);
+        txtWeatherInfo = (TextView) findViewById(R.id.weatherInfo);
+        txtTemperature = (TextView) findViewById(R.id.temperature);
+        txtDescription = (TextView) findViewById(R.id.discription);
+        igvWeatherIcon = (ImageView) findViewById(R.id.weatherIcon);
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver, new IntentFilter("weatherMessage"));
+        LocalBroadcastManager.getInstance(this).registerReceiver(JSONReceiver, new IntentFilter("JSONIntent"));
+        LocalBroadcastManager.getInstance(this).registerReceiver(DrawableReceiver, new IntentFilter("DrawableIntent"));
 
     }
 
@@ -54,20 +74,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onButtonClick(View v) {
-        if (mBound) {
-
-            mService.startWeatherRequest();
-
-        }
-    }
-
-    private BroadcastReceiver myReceiver = new BroadcastReceiver() {
+    private BroadcastReceiver JSONReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.i("MainUI", "Broadcast received");
-            String mystring = intent.getExtras().getString("JSONString");
-            txtview.setText(mystring);
+            Log.i("MainUI", "Broadcast JSON received");
+            String JSON = intent.getExtras().getString("JSONString");
+            updateView(JSON);
+        }
+    };
+
+    private BroadcastReceiver DrawableReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("MainUI", "Broadcast Icon received");
+            Bitmap drawable = (Bitmap) intent.getParcelableExtra("Drawable");
+            igvWeatherIcon.setImageBitmap(drawable);
         }
     };
 
@@ -78,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
             WeatherService.LocalBinder binder = (WeatherService.LocalBinder) iBinder;
             mService = binder.getService();
             mBound = true;
-            mService.startServiceHandler(10000);
+            mService.startServiceHandler(1000*60*5);
         }
 
         @Override
@@ -86,4 +107,43 @@ public class MainActivity extends AppCompatActivity {
             mBound = false;
         }
     };
+
+    public void updateView(String JSON){
+        try{
+
+            Log.i("MainUI", "updateView");
+
+            JSONObject jsonObject = new JSONObject(JSON);
+
+            JSONObject weather = jsonObject.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = jsonObject.getJSONObject("main");
+            JSONObject wind = jsonObject.getJSONObject("wind");
+            JSONObject rain = jsonObject.getJSONObject("rain");
+            DateFormat df = new SimpleDateFormat("HH.mm.ss '-' dd.MM.yyyy");
+
+            txtCity.setText("Århus");
+
+            txtUpdate.setText("Last Updated: " + df.format(Calendar.getInstance().getTime()));
+
+            txtTemperature.setText(String.format("%.0f", main.getDouble("temp")) + " ℃");
+
+            txtDescription.setText(weather.getString("description").toUpperCase());
+
+            txtWeatherInfo.setText(
+                    "Wind Speed: " + wind.getString("speed") + " m/s" + "\n" +
+                            "Humidity: " + main.getString("humidity") + " %" + "\n" +
+                            "Pressure: " + main.getString("pressure") + " hPa" + "\n" +
+                            "Rain: " + rain.getString("3h")
+            );
+
+
+
+            mService.startIconRequest("http://openweathermap.org/img/w/" + weather.getString("icon") + ".png");
+
+
+        } catch (JSONException ex){
+            Log.i("MainUI", "updateView Exception");
+        }
+
+    }
 }
--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
@@ -3,6 +3,9 @@ package com.example.frederikandersen.h4_group_4;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -12,8 +15,12 @@ import android.os.Looper;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -27,7 +34,6 @@ import java.util.logging.LogRecord;
 public class WeatherService extends Service {
 
     private String HTTP_ADDRESS = "http://api.openweathermap.org/data/2.5/weather?id=2624652&units=metric";
-
 
     private final IBinder mBinder = new LocalBinder();
 
@@ -72,8 +78,24 @@ public class WeatherService extends Service {
         @Override
         protected void onPostExecute(String result){
             Log.i("weatherService", "onPostExecute");
-            Intent broadcastIntent = new Intent("weatherMessage");
+            Intent broadcastIntent = new Intent("JSONIntent");
             broadcastIntent.putExtra("JSONString", result);
+            LocalBroadcastManager.getInstance(WeatherService.this).sendBroadcast(broadcastIntent);
+        }
+    }
+
+    private class DownloadweatherIcon extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls){
+            Log.i("Download icon", "doInBackground");
+            return LoadImageFromWebOperations(urls[0]);
+        }
+        @Override
+        protected void onPostExecute(Bitmap result){
+            Log.i("Download icon", "onPostExecute");
+            Intent broadcastIntent = new Intent("DrawableIntent");
+            broadcastIntent.putExtra("Drawable", result);
             LocalBroadcastManager.getInstance(WeatherService.this).sendBroadcast(broadcastIntent);
         }
     }
@@ -82,6 +104,28 @@ public class WeatherService extends Service {
         Log.i("weatherService", "startWeatherRequest");
         new DownloadweatherData().execute(HTTP_ADDRESS);
 
+    }
+
+    public void startIconRequest(String url){
+        Log.i("weatherService", "startIconRequest");
+        new DownloadweatherIcon().execute(url);
+    }
+
+    public static Bitmap LoadImageFromWebOperations(String url) {
+        try {
+            Log.i("LoadImageFromWebsite", "start");
+            //InputStream is = (InputStream) new URL(url).getContent();
+            Bitmap image = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            //Drawable d = Drawable.createFromStream(is, "src name");
+            return image;
+        } catch (MalformedURLException ex) {
+            Log.i("LoadImageFromWebsite", " mal exception");
+            return null;
+        }
+        catch (IOException ex ){
+            Log.i("LoadImageFromWebsite", " io exception");
+            return null;
+        }
     }
 
     public String getWeatherJSON(String myURL) {
@@ -102,15 +146,16 @@ public class WeatherService extends Service {
                stringBuilder.append((line+"\n"));
            }
            bufferedReader.close();
+
            return stringBuilder.toString();
        }
        catch (MalformedURLException ex) {
            Log.i("getWeatherJSON", "MalformedURLException");
-           return "";
+           return null;
        }
        catch (IOException ex){
            Log.i("getWeatherJSON", "IOException");
-           return "";
+           return null;
        }
    }
 
