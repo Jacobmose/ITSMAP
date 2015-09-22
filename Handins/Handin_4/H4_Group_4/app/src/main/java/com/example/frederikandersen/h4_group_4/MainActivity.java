e3b246a39ece6e83c096d9dd6f68698c1c040246
Frederik Andersen <frederik@fafi.dk>
2015-09-22 11:11:19 +0200

    Background code done for Handin_4

--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.frederikandersen.h4_group_4;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtview = (TextView) findViewById(R.id.textView);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver, new IntentFilter("weatherMessage"));
+
     }
 
     @Override
@@ -33,10 +42,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, WeatherService.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
-        //String mystring = mService.getWeatherJSON();
-
-
-        //txtview.setText(mystring);
     }
 
     @Override
@@ -49,12 +54,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onButtonClick(View v) {
+        if (mBound) {
+
+            mService.startWeatherRequest();
+
+        }
+    }
+
+    private BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("MainUI", "Broadcast received");
+            String mystring = intent.getExtras().getString("JSONString");
+            txtview.setText(mystring);
+        }
+    };
+
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.i("ServiceConnection", "OnServiceConnection");
             WeatherService.LocalBinder binder = (WeatherService.LocalBinder) iBinder;
             mService = binder.getService();
             mBound = true;
+            mService.startServiceHandler(10000);
         }
 
         @Override
--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
@@ -1,9 +1,15 @@
 package com.example.frederikandersen.h4_group_4;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Looper;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -13,29 +19,80 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.MalformedInputException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class WeatherService extends Service {
 
     private String HTTP_ADDRESS = "http://api.openweathermap.org/data/2.5/weather?id=2624652&units=metric";
 
+
     private final IBinder mBinder = new LocalBinder();
 
     public class LocalBinder extends Binder{
         WeatherService getService(){
+            Log.i("WeatherService", "getService");
             return WeatherService.this;
         }
     }
 
-   public String getWeatherJSON() {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public void startServiceHandler(long timeInMilliSec){
+        final android.os.Handler handler = new android.os.Handler();
+
+        Timer timer = new Timer();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        startWeatherRequest();
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, timeInMilliSec);
+    }
+
+    private class DownloadweatherData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls){
+            Log.i("weatherService", "doInBackground");
+            return getWeatherJSON(urls[0]);
+        }
+        @Override
+        protected void onPostExecute(String result){
+            Log.i("weatherService", "onPostExecute");
+            Intent broadcastIntent = new Intent("weatherMessage");
+            broadcastIntent.putExtra("JSONString", result);
+            LocalBroadcastManager.getInstance(WeatherService.this).sendBroadcast(broadcastIntent);
+        }
+    }
+
+    public void startWeatherRequest(){
+        Log.i("weatherService", "startWeatherRequest");
+        new DownloadweatherData().execute(HTTP_ADDRESS);
+
+    }
+
+    public String getWeatherJSON(String myURL) {
 
        HttpURLConnection connection = null;
 
        try {
-           URL url = new URL(HTTP_ADDRESS);
+           URL url = new URL(myURL);
            connection = (HttpURLConnection) url.openConnection();
            connection.setRequestMethod("GET");
            connection.connect();
-
 
            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
            StringBuilder stringBuilder = new StringBuilder();
@@ -55,11 +112,6 @@ public class WeatherService extends Service {
            Log.i("getWeatherJSON", "IOException");
            return "";
        }
-
    }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
-    }
 }
