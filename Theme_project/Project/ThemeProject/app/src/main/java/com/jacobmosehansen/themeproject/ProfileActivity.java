0d3d34641c5dadceee043702839e91903a0172bd
Morten Morberg Madsen <Morten3m@gmail.com>
2015-09-25 19:51:47 +0200

    Updated profile_activity - +rating +select subject + defeault profile picture
    // Morten

--- Theme_project/Project/ThemeProject/app/src/main/java/com/jacobmosehansen/themeproject/ProfileActivity.java
+++ Theme_project/Project/ThemeProject/app/src/main/java/com/jacobmosehansen/themeproject/ProfileActivity.java
@@ -7,10 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
-import android.widget.Toast;
-
+import android.widget.Spinner;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -18,26 +18,33 @@ public class ProfileActivity extends AppCompatActivity {
 
     ImageView ivProfilePicture;
     RatingBar rbGradRating;
+    Spinner sprSubjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        // set round profile picture //
+        // Set Round Profile Picture //
         ivProfilePicture = (ImageView) findViewById(R.id.imageView_profilePicture);
-        Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.profile);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.default_profile);
         roundImage = new RoundImage(bm);
         ivProfilePicture.setImageDrawable(roundImage);
 
-        // Set ratingbar //
+        // Set Rating Bar //
         rbGradRating = (RatingBar) findViewById(R.id.ratingBar_profileRating);
-        rbGradRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(getApplicationContext(), Float.toString(rating), Toast.LENGTH_LONG).show();
-            }
-        });
+
+        // Set Subject Spinner //
+        sprSubjects = (Spinner) findViewById(R.id.spinner_subjects);
+        ArrayAdapter<CharSequence> myAdapter = ArrayAdapter.createFromResource(this,
+                R.array.subjects_array, android.R.layout.simple_spinner_item);
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sprSubjects.setPrompt("Select a subject");
+
+        // Set Subject Selection Preset Text //
+        sprSubjects.setAdapter(new NothingSelectedSpinnerAdapter(myAdapter,
+                R.layout.spinner_row_nothing_selected,this));
+
 
     }
 
