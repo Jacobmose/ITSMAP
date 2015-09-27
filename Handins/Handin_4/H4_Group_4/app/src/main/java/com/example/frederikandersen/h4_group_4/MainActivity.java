a12a3a3045c13c84bcfa701ecff4747d77300abd
Frederik Andersen <frederik@fafi.dk>
2015-09-27 23:49:33 +0200

    Handin_4 done

--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/MainActivity.java
@@ -12,23 +12,14 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
-import java.text.FieldPosition;
-import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -75,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop(){
-        super.onStop();
+    protected void onDestroy(){
+        super.onDestroy();
 
         if(mBound) {
             unbindService(mConnection);
--- Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
+++ Handins/Handin_4/H4_Group_4/app/src/main/java/com/example/frederikandersen/h4_group_4/WeatherService.java
@@ -1,22 +1,14 @@
 package com.example.frederikandersen.h4_group_4;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.Looper;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,17 +17,16 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.MalformedInputException;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class WeatherService extends Service {
 
     private String HTTP_ADDRESS = "http://api.openweathermap.org/data/2.5/weather?id=2624652&units=metric";
 
     private final IBinder mBinder = new LocalBinder();
+
+    private Timer timer;
 
     public class LocalBinder extends Binder{
         WeatherService getService(){
@@ -49,10 +40,15 @@ public class WeatherService extends Service {
         return mBinder;
     }
 
+    @Override
+    public void onDestroy(){
+        timer.cancel();
+    }
+
     public void startServiceHandler(long timeInMilliSec){
         final android.os.Handler handler = new android.os.Handler();
 
-        Timer timer = new Timer();
+        timer = new Timer();
 
         TimerTask timerTask = new TimerTask() {
             @Override
@@ -68,7 +64,7 @@ public class WeatherService extends Service {
         timer.schedule(timerTask, 0, timeInMilliSec);
     }
 
-    private class DownloadweatherData extends AsyncTask<String, Void, String> {
+    private class DownloadWeatherData extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... urls){
@@ -102,7 +98,7 @@ public class WeatherService extends Service {
 
     public void startWeatherRequest(){
         Log.i("weatherService", "startWeatherRequest");
-        new DownloadweatherData().execute(HTTP_ADDRESS);
+        new DownloadWeatherData().execute(HTTP_ADDRESS);
 
     }
 
