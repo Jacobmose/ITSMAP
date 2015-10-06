dbaeec2c7d358ff0fce146407c5c78eb70abbfed
Morten Morberg Madsen <Morten3m@gmail.com>
2015-10-06 11:55:10 +0200

    prepared fragments for profile

---
+++ Theme_project/Project/ThemeProject/app/src/main/java/com/jacobmosehansen/themeproject/Profile/AnotherProfileFragment.java
@@ -0,0 +1,15 @@
+package com.jacobmosehansen.themeproject.Profile;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Morten on 06-10-2015.
+ */
+public class AnotherProfileFragment extends Fragment {
+
+
+}
---
+++ Theme_project/Project/ThemeProject/app/src/main/java/com/jacobmosehansen/themeproject/Profile/OwnProfileFragment.java
@@ -0,0 +1,17 @@
+package com.jacobmosehansen.themeproject.Profile;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by Morten on 06-10-2015.
+ */
+public class OwnProfileFragment extends Fragment
+{
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+}
